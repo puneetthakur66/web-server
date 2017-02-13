cf06a7802a595cbc6fa12e39defe1045837c59a2
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-02-13 17:28:38 +0530

    testing final

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', function (req, res) {
-	res.send('This is about us PAGE !');
+	res.send('This is about us page!');
 });
 
 app.use(express.static(__dirname + '/public'));
