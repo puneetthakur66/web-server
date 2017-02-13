29662ac2f3f3c2a0179f02b8f0f48abc4931bf02
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-02-13 17:25:33 +0530

    Initial Repository

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Private Route Hit');
+		next();
+	},
+	logger: function (req, res, next) {
+		var date = new Date().toString();
+		console.log(req.method + " " + req.originalUrl + " on: " + date);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', function (req, res) {
+	res.send('This is about us page');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port' + PORT);
+});
+
